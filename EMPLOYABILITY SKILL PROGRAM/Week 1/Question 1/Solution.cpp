6164d76aa5df4a6c3b1df0d863f1e3ad9d8bb09d
pankaj-creator <68644198+pankaj-creator@users.noreply.github.com>
2020-12-28 22:38:49 +0530

    update

--- EMPLOYABILITY SKILL PROGRAM/Week 1/Question 1/Solution.cpp
+++ EMPLOYABILITY SKILL PROGRAM/Week 1/Question 1/Solution.cpp
@@ -1,12 +1,18 @@
 #include <stdio.h>
-#include <iostrem>
+#include<iostream>
 
-using namespace
-
-void main()
+using namespace std;
+int main()
 {
-int m,n,i,j,chain;
+    int num1, num2;
+    cout<<"Enter the Two Numbers: ";
+    cin>>num1>>num2;
+    if (num1 % num2 == 0) // checks to see if number1 is a multiple of number2
+         cout << "true"; // if so then print out "true"
 
+         else
+         cout << "false";
 
-
+    cout<<endl;
+    return 0;
 }
